ad9a50ff075c37432d0f99d838b1ef71165a732c
Harshini <hs0134@srmist.edu.in>
2026-02-17 09:36:05 +0530

    [Harshini] added:UC5-Print OOPS

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,21 @@
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
 
-        System.out.println("   ***     ***    ******    *****");
-        System.out.println("  ** **   ** **   **   **  **   **");
-        System.out.println(" **   ** **   **  **   **  **");
-        System.out.println(" **   ** **   **  ******    *****");
-        System.out.println(" **   ** **   **  **             **");
-        System.out.println("  ** **   ** **   **       **   **");
-        System.out.println("   ***     ***    **        *****");
+        // Inline Array Declaration + Initialization using String.join()
+        String[] banner = {
+                String.join("", " *****    *****   *****    ***** "),
+                String.join("", "*     *  *     *  *     *  *     *"),
+                String.join("", "*     *  *     *  *     *  *"),
+                String.join("", "*     *  *     *  *****    ***** "),
+                String.join("", "*     *  *     *  *             *"),
+                String.join("", "*     *  *     *  *       *     *"),
+                String.join("", " *****    *****   *        ***** ")
+        };
 
+        // Enhanced For Loop
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
